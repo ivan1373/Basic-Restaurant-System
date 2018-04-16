322f40f9dbed6d12fb26b38f06a9164d66451a15
ivan1373 <ivan.milos2009@gmail.com>
2018-04-16 14:23:16 +0200

    ...

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,7 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
-use Illuminate\Support\Facades\URL;
+//use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -14,10 +14,10 @@ class AppServiceProvider extends ServiceProvider
      * @return void
      */
 
-     if ($this->app->environment() == 'production') {
+  /*   if ($this->app->environment() == 'production') {
     URL::forceScheme('https');
 }
-
+*/
     public function boot()
     {
         //
